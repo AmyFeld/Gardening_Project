d1acef5ef217713cb6e19c986d278b48438efcfd
AmyFeld <58443453+AmyFeld@users.noreply.github.com>
2020-04-20 15:12:58 -0400

    Update View.java

--- Final Project/src/View.java
+++ Final Project/src/View.java
@@ -1,4 +1,70 @@
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
 
 public class View {
+  
+  	//[] Image img;
+	
+	// value of the height and width of screen
+		int canvasWidth = 1380;
+		int canvasHeight = 940;
+		// value of the size of the image
+		static final int imgWidthOrig = 100;
+		static final int imgHeightOrig = 100;
+
+		int imgWidth = 300;
+		int imgHeight = 300;
+	    
+	    GraphicsContext gc;
+
+	    Image background;
+		// array of wide png images
+	    Image[] animationSequence;
+
+		
+		
+		//variables to determine the location of image
+		double x = 0;
+		double y = 0;
+		
+		//double xMouse;
+		//double yMouse;
+		
+	
+	/*
+	 * Input: none
+	 * Output: 
+	 * Function: 
+	 */
+	public void uploadImage() {
+		
+	}
+	
+	/*
+	 * Input: 
+	 * Output: 
+	 * Function: 
+	 */
+	public void update() {
+		
+	}
+	
+	/*
+	 * Input: 
+	 * Output: 
+	 * Function: 
+	 */
+	public void updatePlant(Plant p) {
+		
+	}
+	
+	/*
+	 * Input: 
+	 * Output: 
+	 * Function: 
+	 */
+	public void ()
+	
 
 }
