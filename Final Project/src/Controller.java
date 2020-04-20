757555832c80a11d05009ce379d00b3431aa3094
tmfish13 <48036533+tmfish13@users.noreply.github.com>
2020-04-20 15:13:13 -0400

    Update Controller.java

--- Final Project/src/Controller.java
+++ Final Project/src/Controller.java
@@ -1,4 +1,29 @@
+import javafx.stage.Stage;
 
 public class Controller {
-
+	View view;
+	Model model;
+	
+	Controller(){
+		this.view = new view1;
+		this.model = new Model();
+	}
+	
+	@Override
+	public void start(Stage theStage) {
+		model.updateGarden();
+		view.update();
+		
+		EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent e) {
+				
+			}
+		}
+		view.b.setOnAction(event);
+		
+	}
+	
+	public static void main(String[] args) {
+        launch(args);
+    }
 }
