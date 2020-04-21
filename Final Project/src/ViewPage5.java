808e7cd68f2bfe665d6ee778b3cf268beacad9c8
AmyFeld <58443453+AmyFeld@users.noreply.github.com>
2020-04-21 11:23:36 -0400

    Update ViewPage5.java

--- Final Project/src/ViewPage5.java
+++ Final Project/src/ViewPage5.java
@@ -1,9 +1,84 @@
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
-public class ViewPage5{
+public class ViewPage5 extends View{
+	String title;
+	
+
+	
+	public ViewPage5(Stage theStage, EventHandler<ActionEvent> SSButton) {
+        theStage.setTitle("Rate My Garden");
+
+       
+        
+        Group root = new Group();
+        Scene theScene = new Scene(root);
+        theStage.setScene(theScene);
+
+        Canvas canvas = new Canvas(canvasWidth, canvasHeight);
+        root.getChildren().add(canvas);
+
+
+        gc = canvas.getGraphicsContext2D();
+
+		
+		//importImages();
+		
+	
+	}
+
+  /*
+   * Input: int 
+   * Output: None
+   * Function: presents happiness using some form of icon
+   */
+  public void presentHappiness(int happy){
+
+  }
   
-  ViewPage5{
+  /*
+   * Input: int 
+   * Output: None
+   * Function: presents cont bloom rating using some form of icon
+   */
+  public void presentContBloom(int bloom){
+    
+  }
+  
+    /*
+   * Input: int 
+   * Output: None
+   * Function: presents animals fed rating  using some form of icon
+   */
+  
+  public void presentAnimalsFedRating(int animal){
   
   }
+  
+    /*
+   * Input: int 
+   * Output: None
+   * Function: presents compatibility rating using some form of icon
+   */
+  public void presentCompatibilityRating(int compat){
+    
+  }
+  
+    /*
+   * Input: int 
+   * Output: None
+   * Function: presents transition rating using some form of icon
+   */
+  
+  public void presentTransition Rating(int transit){
+    
+  }
+
   
   /*
    * Input: None 
