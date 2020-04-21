65de2d3dce29d2f3244929c8df785f1a5f3ef91e
tmfish13 <48036533+tmfish13@users.noreply.github.com>
2020-04-21 15:41:24 -0400

    Update ModelTest.java

--- Final Project/src/ModelTest.java
+++ Final Project/src/ModelTest.java
@@ -1,1 +1,14 @@
-//ah
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+
+public class ModelTest {
+	
+	Garden G = new Garden();
+   
+   @Test
+   public void testRateGarden() {	
+      System.out.println("Testing rateGarden");    
+      assertEquals(0, rateGarden(G));     
+   }
+}
