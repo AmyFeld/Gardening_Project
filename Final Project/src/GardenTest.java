48a6f9d77e1a8b37ce1cc776fc02cd7823666f1f
tmfish13 <48036533+tmfish13@users.noreply.github.com>
2020-04-21 16:15:54 -0400

    Update GardenTest.java

--- Final Project/src/GardenTest.java
+++ Final Project/src/GardenTest.java
@@ -11,6 +11,31 @@ public class GardenTest {
   @Test
   public void testCalculateHappiness(){
 	  System.out.println("Testing HappinessRating");
-	  assert.equals(0, calculateHappinessRating(G));
+	  assertEquals(0, calculateHappinessRating(G));
   }
+
+  @Test
+  public void testCalculateContBloomRating(){
+	  System.out.println("Testing ContBloomRating");
+	  assertEquals(0, calculateContBloomRating(G));
+  }
+	
+  @Test
+  public void testCalculateAnimalsFedRating(){
+	  System.out.println("Testing AnimalsFedRating");
+	  assertEquals(0, calculateAnimalsFedRating(G));
+  }
+
+  @Test
+  public void testCalculateCompatibilityRating(){
+	  System.out.println("Testing CompatibilityRating");
+	  assertEquals(0, calculateCompatibilityRating(G));
+  }
+	
+  @Test
+  public void testCalculateTransitionRating(){
+	  System.out.println("Testing TransitionRating");
+	  assertEquals(0, calculateTransitionRating(G));
+  }	
+	
 }
