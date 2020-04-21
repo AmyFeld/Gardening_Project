01453883925918207089f877e91bf850fbb55f8c
tmfish13 <48036533+tmfish13@users.noreply.github.com>
2020-04-21 15:50:29 -0400

    Update ModelTest.java

--- Final Project/src/ModelTest.java
+++ Final Project/src/ModelTest.java
@@ -5,10 +5,23 @@ import static org.junit.Assert.assertEquals;
 public class ModelTest {
 	
 	Garden G = new Garden();
+	Plant p = new Plant();
    
-   @Test
-   public void testRateGarden() {	
-      System.out.println("Testing rateGarden");    
-      assertEquals(0, rateGarden(G));     
-   }
+   	@Test
+   	public void testRateGarden() {	
+      		System.out.println("Testing rateGarden");    
+      		assertEquals(0, rateGarden(G));     
+   	}
+	
+	@Test
+   	public void testGetGarden() {	
+      		System.out.println("Testing getGarden"); 
+      		assertEquals(this.Garden, getGarden());     
+   	}
+	
+	@Test
+   	public void testGetPlant() {	
+      		System.out.println("Testing getPlant");    
+      		assertEquals(this.plant, getPlant());     
+   	}
 }
