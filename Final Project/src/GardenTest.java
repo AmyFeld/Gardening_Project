1575896fbf1465469ceaccafff663b5719dc6cd6
tmfish13 <48036533+tmfish13@users.noreply.github.com>
2020-04-21 16:09:58 -0400

    Update GardenTest.java

--- Final Project/src/GardenTest.java
+++ Final Project/src/GardenTest.java
@@ -1,1 +1,14 @@
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
 
+// The class is meant to test non void functions found in the Garden class
+
+public class GardenTest {
+	
+	Garden G = new Garden();
+  
+  @Test
+  public void test
+    
+}
