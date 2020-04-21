be11633e7b6132550b82dbd8fc4fafa1997e46b7
AmyFeld <58443453+AmyFeld@users.noreply.github.com>
2020-04-21 15:35:38 -0400

    Update ViewPage4.java

--- Final Project/src/ViewPage4.java
+++ Final Project/src/ViewPage4.java
@@ -1,4 +1,34 @@
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
-public class ViewPage4{
+public class ViewPage4 extends View{
+	String title;
+	
 
+	
+	public ViewPage4(Stage theStage, EventHandler<ActionEvent> SSButton) {
+        theStage.setTitle("My Garden");
+
+       
+        
+        Group root = new Group();
+        Scene theScene = new Scene(root);
+        theStage.setScene(theScene);
+
+        Canvas canvas = new Canvas(canvasWidth, canvasHeight);
+        root.getChildren().add(canvas);
+
+
+        gc = canvas.getGraphicsContext2D();
+
+		
+		//importImages();
+		
+	
+	}
 }
