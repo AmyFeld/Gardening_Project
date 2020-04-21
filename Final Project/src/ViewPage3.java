e62e6d5928297b00c73f326f8a86208e9713044b
AmyFeld <58443453+AmyFeld@users.noreply.github.com>
2020-04-21 15:13:18 -0400

    Update ViewPage3.java

--- Final Project/src/ViewPage3.java
+++ Final Project/src/ViewPage3.java
@@ -1,4 +1,51 @@
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
-public class ViewPage3{
+public class ViewPage3 extends View{
+	String title;
+	
 
+	
+	public ViewPage3(Stage theStage, EventHandler<ActionEvent> SSButton) {
+        theStage.setTitle("Rate My Garden");
+    
+      slider = new JSlider();
+      // have to finish uploading information about this
+    
+
+       
+        
+        Group root = new Group();
+        Scene theScene = new Scene(root);
+        theStage.setScene(theScene);
+
+        Canvas canvas = new Canvas(canvasWidth, canvasHeight);
+        root.getChildren().add(canvas);
+
+
+        gc = canvas.getGraphicsContext2D();
+
+		
+		//importImages();
+		
+	
+	}
+  
+  /*
+   * Input: None
+   * Output: None
+   * Function: Changes what happens when the slider is changed 
+   */
+  public void SliderChange(){
+    
+  }
+  
+  
+    
+    //Helpful Tools: https://www.dummies.com/programming/java/how-to-use-sliders-in-java/
 }
