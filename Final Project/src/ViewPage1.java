820aca9224b7eb572007157f5d9602c8c6b762c7
luckyruby1 <44467999+luckyruby1@users.noreply.github.com>
2020-04-24 21:18:38 -0400

    updated viewpage1 with scene

--- Final Project/src/ViewPage1.java
+++ Final Project/src/ViewPage1.java
@@ -4,16 +4,45 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 // This class is a subclass of view that will draw out the home page with proper buttons
 
-public class ViewPage1 extends View{
-	String title;
+public class ViewPage1 extends View {
+	
+	Scene scene1;
+
+	public ViewPage1(Stage theStage) {
+				
+		  // home button if fired
+		VBox layout1 = new VBox(20);     
+	    Label label1 = new Label("Create Your Own Garden");
+	  	Button button1 = new Button("Start Planting");
+	  	layout1.getChildren().addAll(label1, button1);
+		
+	  	scene1 = new Scene(layout1, 600, 300); // the button and label
+		 //   scene1.setFill(Color.LIGHTGREEN);
+	  		  	
+	    button1.setOnAction(e -> theStage.setScene(new ViewPage2(theStage).getScene2()));	 
+	  	
+	   	 theStage.setScene(scene1); 
+	   	 theStage.show(); 	
+
+	}	
+	
+	public Scene getScene1() {
+		return scene1;
+	}
+
+	 public static void main(String args[]){          
+	      launch(args);     
+	   }
 	
 
 	
-	public ViewPage1(Stage theStage, EventHandler<ActionEvent> SSButton) {
+	/*public ViewPage1(Stage theStage, EventHandler<ActionEvent> SSButton) {
         theStage.setTitle("AboutArden");
 
         Button page2  = new Button("Start Gardening");
@@ -52,5 +81,5 @@ public class ViewPage1 extends View{
 		//importImages();
 		
 	
-	}
+	} */
 }
