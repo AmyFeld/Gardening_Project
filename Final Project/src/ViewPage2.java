3aa711087d1d60f2d73e4072cb501b21f0895999
luckyruby1 <44467999+luckyruby1@users.noreply.github.com>
2020-04-25 19:59:29 -0400

    plant nursery has tabs!
    Plant nursery is a tab with 15 buttons. If a button is clicked, a new tab is opened with the correct label

--- Final Project/src/ViewPage2.java
+++ Final Project/src/ViewPage2.java
@@ -1,26 +1,68 @@
-import java.awt.Button;
-import java.beans.EventHandler;
-import java.util.HashMap;
-import java.util.Map;
-
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 // This class is a subclass of view that will draw out the greenery tour page
 
 public class ViewPage2 extends View {
 	Scene scene2;	
+	int count = 0;
+
 	
 	public ViewPage2(Stage theStage) {
-		VBox layout2 = new VBox(20);
+		VBox layout2 = new VBox(20); // assembles the title, tabs, and button vertically
+		FlowPane flow = new FlowPane();
+		
+		flow.setTranslateX(30);
+		flow.setTranslateY(120);
+		
+		flow.setVgap(100);
+	    flow.setHgap(100);
+	    flow.setPrefWrapLength(200);
+		
+	    TabPane tabPane = new TabPane();
+	    
+	    Tab nTab = new Tab(); // main tab
+	    nTab.setText("Nursery Plant Selection");
+
+	    // creates tabs for all the plants linked with button
+	    for (int i = 0; i < 15; i++) {
+	    	Button b1 = new Button("Plant" + ++count); // modify for the correct name of plant
+	    	flow.getChildren().addAll(b1);
+	    	
+			Tab t = new Tab(); 
+			t.setText("Plant" + count);
+	        b1.setOnAction(e -> tabPane.getTabs().addAll(t));
+	
+	    }
+	    
+	    // Tab tab1 = tabPane.getTabs().get(0); // access the tabs individually and add info
+	    // another method is to use a for loop for the arraylist of plant images, names, etc
+	    // gives errors for now (out of bounds)
+	    
+	    
+	    tabPane.getTabs().add(nTab);
+		nTab.setContent(flow);
+
+
 		Label label2 = new Label("Greenery Tour");
-		layout2.getChildren().addAll(label2, homeButton); 
-		
-		homeButton.setOnAction(e -> theStage.setScene(new ViewPage1(theStage).getScene1())); // go back to the original screen
-		
-		scene2 = new Scene(layout2, 600, 300);	
+		label2.setFont(new Font("Arial", 20));
+		label2.setTranslateX(400);
+	 	label2.setTranslateY(10);
+				
+		 layout2.getChildren().addAll(label2, tabPane, homeButton); 
+
+		 
+		 homeButton.setOnAction(e -> theStage.setScene(new ViewPage1(theStage).getScene1())); // go back to the original screen
+		 scene2 = new Scene(layout2, 900, 600);	
 		
 	   	theStage.setScene(scene2); 
 	   	  theStage.show(); 		
@@ -34,11 +76,7 @@ public class ViewPage2 extends View {
 	
 
 	/*
-	Map<String, Plant> plantCollection = new HashMap<String, Plant>();
-	
-	Button bColor  = new Button("Color");
-	// bColor.setOnAction(SSButton);
-	
+	Map<String, Plant> plantCollection = new HashMap<String, Plant>();	
 	
 	/*
 	 * Input: takes in a name
