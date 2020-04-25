6b6699d79e3a98f45de3aa84185039a026e6e818
AmyFeld <58443453+AmyFeld@users.noreply.github.com>
2020-04-25 16:52:57 -0400

    Button added to go back home

--- Final Project/src/ViewPage6.java
+++ Final Project/src/ViewPage6.java
@@ -4,13 +4,48 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
 // This class is used to present the About Arden tab and include the information and buttons that will present the information
 
 public class ViewPage6 extends View{
-	String title;
+	
+	Scene scene1;
+
+	public ViewPage6(Stage theStage) {
+				
+		  // home button if fired
+		VBox layout1 = new VBox(20);     
+	    Label label1 = new Label("About Arden");
+	  	Button home = new Button("Home");
+	  	Button ms = new Button("Mission Statement");
+	  	Button purp = new Button("Purpose");
+	  	Button hist = new Button("History");
+	  	
+	  	layout1.getChildren().addAll(label1, home,ms,purp,hist);
+		
+	  	scene1 = new Scene(layout1, 600, 300); // the button and label
+		 //   scene1.setFill(Color.LIGHTGREEN);
+	  		  	
+	    home.setOnAction(e -> theStage.setScene(new ViewPage1(theStage).getScene1()));	 
+	  	
+	   	 theStage.setScene(scene1); 
+	   	 theStage.show(); 	
+
+	}	
+	
+	public Scene getScene6() {
+		return scene1;
+	}
+
+	 public static void main(String args[]){          
+	      launch(args);     
+	   }
+	
+	/*String title;
 	
 
 	
@@ -43,6 +78,7 @@ public class ViewPage6 extends View{
 		
 	
 	}
+	*/
 
 	/*
 	 * Input: None
