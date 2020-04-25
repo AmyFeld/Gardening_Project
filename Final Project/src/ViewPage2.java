42bfe0fd9bf5ed64c7223dab3ef57e49d56513f2
luckyruby1 <44467999+luckyruby1@users.noreply.github.com>
2020-04-24 21:19:07 -0400

    updated viewpage2 with scene

--- Final Project/src/ViewPage2.java
+++ Final Project/src/ViewPage2.java
@@ -3,18 +3,37 @@ import java.beans.EventHandler;
 import java.util.HashMap;
 import java.util.Map;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 // This class is a subclass of view that will draw out the greenery tour page
 
-public class ViewPage2 {
+public class ViewPage2 extends View {
+	Scene scene2;	
 	
-	Plant p;
+	public ViewPage2(Stage theStage) {
+		VBox layout2 = new VBox(20);
+		Label label2 = new Label("Greenery Tour");
+		layout2.getChildren().addAll(label2, homeButton); 
+		
+		homeButton.setOnAction(e -> theStage.setScene(new ViewPage1(theStage).getScene1())); // go back to the original screen
+		
+		scene2 = new Scene(layout2, 600, 300);	
+		
+	   	theStage.setScene(scene2); 
+	   	  theStage.show(); 		
 
-	
-	public ViewPage2(Stage theStage, EventHandler<ActionEvent> SSButton) {
-        theStage.setTitle("Rate My Garden");
+		
 	} 
 	
+	public Scene getScene2() {
+		return scene2;
+	}
 	
+
+	/*
 	Map<String, Plant> plantCollection = new HashMap<String, Plant>();
 	
 	Button bColor  = new Button("Color");
@@ -25,7 +44,7 @@ public class ViewPage2 {
 	 * Input: takes in a name
 	 * Output: none
 	 * Function: the chosen plant is added to the collection
-	 */
+	 
 	public void chosenPlant(String name) {		
 		Plant plant = getPlant(name);
 		plantCollection.put(name, plant);
@@ -36,7 +55,7 @@ public class ViewPage2 {
 	 * Input: the color
 	 * Output: string of color
 	 * Function: sorts on the color of the plant 
-	 */
+	 
 	public String selectColor(String color) {
 		//iterate through icons of color and displays plants of color selected
 	}
@@ -45,12 +64,12 @@ public class ViewPage2 {
 	 * Input: none
 	 * Output: none
 	 * Function: getter for the plant 
-	 */
+	 
 	public Plant getPlant() {
 		return p;
 	}
 
-	
+	*/
 	
 	 
 
