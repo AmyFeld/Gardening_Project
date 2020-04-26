f52bbb2db14ea16ab51779bf235b7caf4d93797e
luckyruby1 <44467999+luckyruby1@users.noreply.github.com>
2020-04-26 16:32:36 -0400

    Updated with: animals.txt, edge.txt, appear.txt
    in textFiles folder

--- Final Project/src/ViewPage8.java
+++ Final Project/src/ViewPage8.java
@@ -1,86 +1,90 @@
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Group;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 // This class is used to present the Benefits of a Garden tab and include the information and buttons that will present the information
 
-public class ViewPage8 extends View{
-	Scene scene1;
+public class ViewPage8 extends View {
+	Scene scene8;
 
 	public ViewPage8(Stage theStage) {
-				
-		  // home button if fired
-		VBox layout1 = new VBox(20);     
-	    Label label1 = new Label("Benefits of a Garden");
-	  	Button home = new Button("Home");
-	  	Button animal = new Button("Animals");
-	  	Button stop = new Button("Stopping an Edge");
-	  	
-	  	
-	  	layout1.getChildren().addAll(label1, home,animal,stop);
 		
-	  	scene1 = new Scene(layout1, 600, 300); // the button and label
-		 //   scene1.setFill(Color.LIGHTGREEN);
-	  		  	
-	    home.setOnAction(e -> theStage.setScene(new ViewPage1(theStage).getScene1()));	 
+		VBox layout = new VBox(20);     
+	    Label label = new Label("Benefits of a Garden");
+	    Text animalText = new Text();
+	    Text edgeText = new Text();
+	    Text appearText = new Text();
+	    
+	    try {
+			animalText.setText(uploadText("animals"));
+		} catch (Exception e2) {
+			e2.printStackTrace();
+		}
+	    
+	    try {
+			edgeText.setText(uploadText("edge"));
+		} catch (Exception e2) {
+			e2.printStackTrace();
+		}
+	    try {
+			appearText.setText(uploadText("appear"));
+		} catch (Exception e2) {
+			e2.printStackTrace();
+		}
+	    
+
+	  	Button home = new Button("Home");
+	    home.setOnAction(e -> theStage.setScene(new ViewPage1(theStage).getScene1()));
+
+	  	Tab animals = new Tab();
+	  	animals.setText("Animals");
+	  	animals.setContent(animalText);
 	  	
-	   	 theStage.setScene(scene1); 
+	  	Tab edge = new Tab();
+	  	edge.setText("Stopping An Edge");
+	  	edge.setContent(edgeText);
+	  	
+		Tab appear = new Tab();
+	  	appear.setText("Aesthetics and Appearances");
+	  	appear.setContent(appearText);
+
+		TabPane tabPane = new TabPane();
+		tabPane.getTabs().addAll(animals, edge, appear);
+		
+	  	layout.getChildren().addAll(label, tabPane, home); 
+	  	scene8 = new Scene(layout, 900, 600); 
+		  	
+	   	 theStage.setScene(scene8); 
 	   	 theStage.show(); 	
 
 	}	
 	
 	public Scene getScene8() {
-		return scene1;
+		return scene8;
 	}
 
 	 public static void main(String args[]){          
 	      launch(args);     
 	   }
 	
-	
-	/*String title;
-	
-
-	
-	public ViewPage8(Stage theStage, EventHandler<ActionEvent> SSButton) {
-        theStage.setTitle("Benefits of a Garden");
-
-        Button animals = new Button("Animals");
-        animals.setOnAction(SSButton);
-        
-        Button edge = new Button("Stopping an Edge");
-        edge.setOnAction(SSButton);
-        
-        Group root = new Group();
-        Scene theScene = new Scene(root);
-        theStage.setScene(theScene);
-
-        Canvas canvas = new Canvas(canvasWidth, canvasHeight);
-        root.getChildren().add(canvas);
-        root.getChildren().add(animals);
-        root.getChildren().add(edge);
-        gc = canvas.getGraphicsContext2D();
-
-	
-		//importImages();
-		
-	
-	}*/
-	
-	/*
-	 * Input: None
-	 * Output: None
-	 * Function: Will read in a .txt file and upload it onto the canvas
-	 */
-	
-public void uploadText() {
-		
-	}
+	 
+		/* Input: None
+		 * Output: None
+		 * Function: Will read in a .txt file and upload it onto the canvas
+		 */
+		 public String uploadText(String name) throws Exception {
+			 String file = new String("textFiles/" + name + ".txt"); 
+			 String data = ""; 
+			 
+			 data = new String(Files.readAllBytes(Paths.get(file))); 
+			 return data;
+		  }
 
 }
