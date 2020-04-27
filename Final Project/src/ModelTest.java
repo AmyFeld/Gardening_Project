cb044937d92a0ab2415b510256b9f73419414629
tmfish13 <48036533+tmfish13@users.noreply.github.com>
2020-04-26 21:25:12 -0400

    redid tests

--- Final Project/src/ModelTest.java
+++ Final Project/src/ModelTest.java
@@ -6,24 +6,26 @@ import static org.junit.Assert.assertEquals;
 
 public class ModelTest {
 	
+	Model model = new Model();
+	
 	Garden G = new Garden();
 	Plant p = new Plant();
    
    	@Test
    	public void testRateGarden() {	
       		System.out.println("Testing rateGarden");    
-      		assertEquals(0, rateGarden(G));     
+      		assertEquals(0, model.rateGarden(G));     
    	}
 	
 	@Test
    	public void testGetGarden() {	
       		System.out.println("Testing getGarden"); 
-      		assertEquals(this.Garden, getGarden());     
+      		assertEquals(model.currentGarden, model.getGarden());     
    	}
 	
 	@Test
    	public void testGetPlant() {	
       		System.out.println("Testing getPlant");    
-      		assertEquals(this.plant, getPlant());     
+      		assertEquals(model.currentPlant, model.getPlant());     
    	}
 }
