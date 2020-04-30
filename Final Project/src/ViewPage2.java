e2bc20ae38c5edb2fd06773aa7a17a2199518cd4
AmyFeld <58443453+AmyFeld@users.noreply.github.com>
2020-04-30 13:21:01 -0400

    Alpha Code Update

--- Final Project/src/ViewPage2.java
+++ Final Project/src/ViewPage2.java
@@ -1,3 +1,7 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -17,21 +21,35 @@ import javafx.scene.text.Font;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
-// This class is a subclass of view that will draw out the greenery tour page
+// This class is a subclass of view that will draw out the greenery tour page. It also presents the plant 
+// information page when a plant is selected and includes a slider to show back and forth between times
+
+
+/*
+* in plant class, the function getDescr will be used to determine how this class will be presented 
+* the button will also have to set model's current plant model.setCurrenPlant() model.getCurrentPlant().getDesc
+* model.getCurrentPlant().inGarden = True; <-- will add to garden tile pane 
+* getImage -- take image and get name or correspondfing img you want to read in
+*/
 
 public class ViewPage2 extends View {
 	Scene scene2;	
 	int count = 0;
-
-	
+	int plaWidth = 100;
+	int plaHeight = 100;
+		
 	public ViewPage2(Stage theStage) {
 		
-		Image back = new Image("file:bg2.png");
+		Garden g = new Garden();
+		ArrayList<Plant> allPlants = g.allPlants;		
+		ArrayList<Plant> myPlants = new ArrayList<Plant>();
+		
+		Image back = new Image("file:images/bg2.png");
 		ImageView bg = new ImageView(back);
 		System.out.println(Screen.getPrimary().getVisualBounds().getWidth());
 		bg.setFitWidth(Screen.getPrimary().getVisualBounds().getWidth());
 		bg.setFitHeight(Screen.getPrimary().getVisualBounds().getHeight());
-		BackgroundImage myBG= new BackgroundImage(back, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+		BackgroundImage myBG = new BackgroundImage(back, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
 		
 		
 		VBox layout2 = new VBox(20); // assembles the title, tabs, and button vertically
@@ -42,24 +60,26 @@ public class ViewPage2 extends View {
 		flow.setTranslateX(30);
 		flow.setTranslateY(120);
 		
-		flow.setVgap(100);
-	    flow.setHgap(100);
+		flow.setVgap(50);
+	    flow.setHgap(50);
 	    flow.setPrefWrapLength(200);
 		
 	    TabPane tabPane = new TabPane();
 	    
 	    Tab nTab = new Tab(); // main tab
 	    nTab.setText("Nursery Plant Selection");
+	    
+	    System.out.println(allPlants.get(0).getImgName());
 
 	    // creates tabs for all the plants linked with button
 	    for (int i = 0; i < 18; i++) {
-	    	Button b1 = new Button("Plant" + ++count); // modify for the correct name of plant
+	    	Button b1 = new Button(allPlants.get(i).getName(), new ImageView(new Image(allPlants.get(i).getImgName(), plaWidth, plaHeight, false, false))); 
 	    	flow.getChildren().addAll(b1);
 	    	//flow.getChildren().add(add);
 	    	
 	    	
 			Tab t = new Tab(); 
-			t.setText("Plant" + count);
+			t.setText(allPlants.get(i).getName());
 			//At.getTabPane().add
 	        b1.setOnAction(e -> tabPane.getTabs().addAll(t));
 	
@@ -83,7 +103,7 @@ public class ViewPage2 extends View {
 		label2.setTranslateX(400);
 	 	label2.setTranslateY(10);
 	 	
-	 	 Button homeButton = new Button("home");
+	 	// Button homeButton = new Button("home");
 	 	
 	 	Button go = new Button("Go to Garden");
 				
@@ -95,7 +115,7 @@ public class ViewPage2 extends View {
 		 
 		 homeButton.setOnAction(e -> theStage.setScene(new ViewPage1(theStage).getScene1())); // go back to the original screen
 		 go.setOnAction(e -> theStage.setScene(new ViewPage4(theStage).getScene4()));//System.out.println("Go Pressed"));//theStage.setScene(new ViewPage4(theStage).getScene4()));
-		 scene2 = new Scene(layout2, 900, 600);	
+		 scene2 = new Scene(layout2, sceneWidth, sceneHeight);	
 		
 	   	theStage.setScene(scene2); 
 	   	  theStage.show(); 		
@@ -107,10 +127,15 @@ public class ViewPage2 extends View {
 		return scene2;
 	}
 	
+	 public String createImage(String name) throws Exception {
+		 String file = new String("plantImg/" + name + ".txt"); 
+		 String data = ""; 
+		 
+		 data = new String(Files.readAllBytes(Paths.get(file))); 
+		 return data;
+	  }
 
-	/*
-	Map<String, Plant> plantCollection = new HashMap<String, Plant>();	
-	
+		
 	/*
 	 * Input: takes in a name
 	 * Output: none
