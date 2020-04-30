5153da52a1983e837edaeefaeefa9ed9fd34da65
AmyFeld <58443453+AmyFeld@users.noreply.github.com>
2020-04-30 19:59:09 -0400

    Update javadoc

--- Final Project/src/Controller.java
+++ Final Project/src/Controller.java
@@ -1,60 +1,47 @@
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.stage.Stage;
 
-// This class is meant to hold the main and communicate between the selected view and model
+/**
+ *  This class is meant to hold the main and communicate between the selected view and model
+ * 
+ * @author Amy Feldman
+ *
+ */
 
-public class Controller extends Application implements MouseListener {
+public class Controller extends Application {
+	
 	View view;
 	Model model;
 	
 	public Controller() {
-
-		this.model = new Model();
 		
-		view.addMouseListener(this);
-		addMouseListener(this);
+	     this.view = new View();	
+       	 this.model = new Model();
 	}
-	
-	/*
-	 * Input: Stage
-	 * Output: running program
-	 * Function: takes in the updates from the model or view and presents to the screen/ user
-	 */
 
+    /**
+     * Begins running the GUI application of the entire program from updates
+     * in model & view
+     * 
+     * @param Stage  a container holding the scene
+     * @throws Exception if an error occurred in Thread.sleep(100)
+     *
+     */
     @Override
-	public void start(Stage theStage) {
-       // view = new View(theStage);
+	public void start(Stage theStage) {	
 		
-		  this.view = new View();
-       		 this.model = new Model();
-		try {
-			this.view.start(theStage);
-		} catch (Exception e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-        
-		//model = new Model(view.getWidth(), view.getHeight(), 
-           //     view.getImageWidth(), view.getImageHeight());
-		
- 	new EventHandler<ActionEvent>() {
+ /*	new EventHandler<ActionEvent>() {
  		@Override 
  		public void handle(ActionEvent event) {
             System.out.print("Hello World !!");
             view.setOnAction(event);
             }		 	     
     };
-    
+    */
     new AnimationTimer() {
-        public void handle(long currentNanoTime) {
-        	
-         
+    	
+        public void handle(long currentNanoTime) { 
             try {
                 Thread.sleep(100);
                 
@@ -62,71 +49,14 @@ public class Controller extends Application implements MouseListener {
                 e.printStackTrace();
             }
         }
+        
     }.start();
     theStage.show();
 }
-    
-    
-	private void addMouseListener(Controller controller) {
-		
-	}
-	
-	/*
-	 * Input: Mouse event
-	 * Output: None
-	 * Function: tells program what to do when mouse is pressed 
-	 */
-	
-	@Override
-	public void mousePressed(MouseEvent e) {
-       		
-    	}
-
-	/*
-	 * Input: Mouse event
-	 * Output: None
-	 * Function: tells program what to do when mouse is released 
-	 */
-	@Override
-    	public void mouseReleased(MouseEvent e) {
-
-    	}
-
-	/*
-	 * Input: Mouse event
-	 * Output: None
-	 * Function: tells program what to do when mouse is entered 
-	 */
-	@Override
-    	public void mouseEntered(MouseEvent e) {
-
-    	}
-
-	/*
-	 * Input: Mouse event
-	 * Output: None
-	 * Function: tells program what to do when mouse is exited 
-	 */
-	@Override
-    	public void mouseExited(MouseEvent e) {
-       
-    	}
-
-	/*
-	 * Input: Mouse event
-	 * Output: None
-	 * Function: tells program what to do when mouse is clicked 
-	 */
-	@Override
-    	public void mouseClicked(MouseEvent e) {
-       
-    	}
-	
+   
 	public static void main(String[] args) {
         System.out.println("WORK");
 		launch(args);
     }
 	
-
-
 }
