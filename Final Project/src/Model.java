316f470e3a3be9ec3713ca81fdc05fd525aca329
tmfish13 <48036533+tmfish13@users.noreply.github.com>
2020-04-30 19:44:12 -0400

    javadoc update

--- Final Project/src/Model.java
+++ Final Project/src/Model.java
@@ -10,37 +10,45 @@ public class Model {
 	Plant currentPlant;
 	ArrayList<Plant> inGarden = new ArrayList<Plant>();
 	
-	/*
-	 * Input: the Garden 
-	 * Output: none
-	 * Function: sets the current garden
+	/**
+	 * Description: This is a setter for the Garden attribute in Model
+	 * 
+	 * @param Garden. used for setting the Model's current Garden
+	 * @return. void
+	 * @throws no exception
 	 */
 	public void setGarden(Garden g){
 		currentGarden = g;
 	}
 	
-	/*
-	 * Input: the plant
-	 * Output: none
-	 * Function: sets the current plant
+	/**
+	 * Description: This is a setter for the Plant attribute in Model
+	 * 
+	 * @param Plant. used for setting the Model's current Plant
+	 * @return. void
+	 * @throws no exception
 	 */
 	public void setPlant(Plant p){
 		currentPlant = p;
 	}
 	
-	/*
-	 * Input: none
-	 * Output: currentPlant
-	 * Function: returns the currentPlant as a getter
+	/**
+	 * Description: This is a getter for the Plant attribute in Model
+	 * 
+	 * @param none
+	 * @return. Plant. the Model's currently selected plant
+	 * @throws no exception
 	 */
 	public Plant getPlant(){
 		return currentPlant;
 	}
 	
-	/*
-	 * Input: none
-	 * Output: currentGarden
-	 * Function: returns the currentGarden as a getter
+	/**
+	 * Description: This is a getter for the Garden attribute in Model
+	 * 
+	 * @param none
+	 * @return. Garden. The model's current Garden
+	 * @throws no exception
 	 */
 	
 	public Garden getGarden(){
@@ -48,24 +56,33 @@ public class Model {
 	}
 	
 	
-	/*
-	 * Input: the Garden class g
-	 * Output: the overall rating as an int
-	 * Function: sets the computer-generated rating and returns an int of the overall
+	/**
+	 * Description: rateGarden takes in the Model's garden and uses all other rate functions to get an average
+	 * star rating for the garden from 1 to 5
+	 * @param Garden. the Model's current Garden
+	 * @return. int. the overall grade for the Garden
+	 * @throws no exception
 	 */
 	public int rateGarden(Garden g) {
 		return 0;
 	}
 	
-	/*
-	 * Input: none
-	 * Output: sets the rating
-	 * Function: the rating is updated from rateGarden()
+	/**
+	 * Description: updateGarden takes the Model's current plant and appends it to the inGarden list of 
+	 * the currentGarden in the Model
+	 * @param void
+	 * @return. void
+	 * @throws no exception
 	 */
 	public void updateGarden() {
 		inGarden.add(currentPlant);
 	}
-	
+	/**
+	 * Description: getPlantByName takes in a string from a view button and finds the corresponding plant in the currentGarden
+	 * @param String. the button's name
+	 * @return. Plant. the plant associated with that name
+	 * @throws no exception
+	 */
 	public Plant getPlantByName(String s) {
 		for (Plant p : this.currentGarden.allPlants) {
 			if(p.name.equals(s)) {
@@ -74,11 +91,22 @@ public class Model {
 		}
 		return null;
 	}
-	
+	/**
+	 * Description: setStartGardeningis the setted for the isGardening boolean
+	 * @param Boolean. the boolean to be set to isGardening
+	 * @return. none
+	 * @throws no exception
+	 */
 	public void setStartGardening(Boolean b){
 	  startGardening = b;
 	}
-	
+	/**
+	 * Description: reset erases the previous garden/plant data from the model and resets the model's attributes back
+	 * to the ones when the model was first initialized.
+	 * @param none
+	 * @return. none
+	 * @throws no exception
+	 */
 	public void reset(){
 		setStartGardening(false);
 		currentPlant = null;
