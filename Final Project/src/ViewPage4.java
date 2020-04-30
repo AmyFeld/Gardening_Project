03f6eaa3c05030e239cb5174a902145cbc6f101c
AmyFeld <58443453+AmyFeld@users.noreply.github.com>
2020-04-30 19:36:32 -0400

    Update javadoc

--- Final Project/src/ViewPage4.java
+++ Final Project/src/ViewPage4.java
@@ -18,6 +18,12 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.TilePane;
 
+/**
+ * This class presents the garden and includes the drag and drop feature to add new items to the garden
+ * 
+ * @author Amy Feldman 
+ *
+ */
 
 //This class presents the garden and includes the drag and drop feature to add new items to the garden
 
@@ -28,6 +34,14 @@ public class ViewPage4 extends View {
 	private TilePane tile;
 	
 
+	/**
+	 * For the user to view the garden and drag & drop, creates the scene that would work
+	 * This would add the buttons as well as and calls the other methods
+	 * 
+	 * @param Stage  Holds the image of the garden and plants
+	 * @return none  Creates an application displaying the graphics 
+	 *
+	 */
 
 	public ViewPage4(Stage theStage) {
 				
@@ -64,6 +78,15 @@ public class ViewPage4 extends View {
 	   	 theStage.show(); 	
 
 	}	
+	
+	/**
+	 * For the user to view the garden and drag & drop
+	 *	for now just creates and adds to the panes to create  ~mock~ garden
+	 * 
+	 * @param Stage  Holds the image of the garden and plants
+	 * @return none  Creates an application displaying the graphics 
+	 *
+	 */
 	public void start(Stage stage) {
 		//Image back = new Image("file:lawn.png", 600, 600, false, false);
 	//	ImageView flowBG = new ImageView(back);
@@ -103,109 +126,19 @@ public class ViewPage4 extends View {
 		
 	}
 	
+	/**
+	 * Description: basic getter for the scene in order to receive it when buttons are pressed on home screens 
+	 * 
+	 * 
+	 * @return scene4
+	 */
 	public Scene getScene4() {
 		return scene4;
 	}
 
-	 public static void main(String args[]){          
+/*	 public static void main(String args[]){          
 	      launch(args);     
-	   }
+	   }*/
 }
 
 
-
-/*
-public class ViewPage4 extends View{
-//private ImgController1 imc1;
-private ImageView iv2;
-private FlowPane flow;
-private BorderPane border;
-private TilePane tile;
-private final double WIDTH = 800;
-private final double HEIGHT = 600;
-Scene scene4;
-/*
-public void MyMovingImageView1(){
-	
-iv2 = new ImageView();
-//imc1 = new ImgController1(this);
-}
-	/*
-	 * Input: stage
-	 * Output: None
-	 * Function: Starts the view and sets the image and creates the basic outline and functioning of the canvas when the screen is initialized to have the panes
-	 */
- 
-/*   @Override
-  public void start(Stage stage) {
-		//code from Lab8 to edit and change for our lab
-  Image im1; //= new Image(getClass().getResourceAsStream("milkWeed.png"), 100, 100, true, true);
- 
-//  iv2.setImage(im1);
-  iv2.setPreserveRatio(true);
-  iv2.setFitHeight(100);
- // iv2.setOnMouseDragged(imc1.getHandlerForDrag());   
-  	tile.setTileAlignment(Pos.CENTER_LEFT);
-      tile.setPrefRows(5);
-      tile.getChildren().add(iv2);
-      
-      flow.setColumnHalignment(HPos.CENTER);
-      flow.setPrefWrapLength(3*WIDTH/4);
-      border.setLeft(tile);
-      border.setRight(flow);
-      
-      
-      Scene scene1 = new Scene(border, WIDTH, HEIGHT);
- 
-      stage.setScene(scene1);
-      
-
-iv2.setOnDragDetected((MouseEvent event) -> {
-	
-  //ImageView iv1 = new ImageView(im1);
-  
-  try {
-		Thread.sleep(500);
-	} catch (InterruptedException e) {
-		e.printStackTrace();
-	}
-  
-	System.out.println("eventX:" + event.getX() + " > tile X:" + 7*tile.getWidth()/12); // debugging
-  
-  if(event.getX() > 7*tile.getWidth()/12) { //boundaries to whether is plant is dragged in flow pane
-  	    	
-  //	flow.getChildren().add(iv1); 
-      //   Dragboard db = iv1.startDragAndDrop(TransferMode.COPY); 
-         ClipboardContent content = new ClipboardContent();
-         
-         // Store node ID in order to know what is dragged.
-       //  content.putString(iv1.getId());
-         //db.setContent(content);
-         event.consume();
-  }
-    
-}); 
-      stage.show();
-  }
-	
-	/*
-	 * Input: double value
-	 * Output: None
-	 * Function: sets the x value of the layout of image view
-	 */
- 
-/*  public void setX(double x) {
-  iv2.setTranslateX(iv2.getLayoutX() - WIDTH/2 + x);
-  }
-	
-	/*
-	 * Input: double value
-	 * Output: None
-	 * Function: sets the y value of the layout of image view
-	 */
- 
- /* public void setY(double y) {
-  iv2.setTranslateY(iv2.getLayoutY() - HEIGHT/2 + y);
-  }
-  */
-//}//}
