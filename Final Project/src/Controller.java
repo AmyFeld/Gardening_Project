e0047b4d9f9bbd744c2835fe58e1b503582c53a8
luckyruby1 <44467999+luckyruby1@users.noreply.github.com>
2020-05-17 12:38:43 -0400

    Fixed parameter of image sizes(plaSize) & changed gridPane to anchorPane

--- Final Project/src/Controller.java
+++ Final Project/src/Controller.java
@@ -1,6 +1,7 @@
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
@@ -17,6 +18,7 @@ public class Controller extends Application {
 	
 	View view;
 	Model model;
+	ViewPage2 vp2;
 	double contX;
 	double contY;
 
@@ -26,6 +28,7 @@ public class Controller extends Application {
 		this.view = view;
 		this.model = new Model();
 	}
+	
 
     /**
      * Begins running the GUI application of the entire program from updates
@@ -37,30 +40,9 @@ public class Controller extends Application {
      */
     @Override
 	public void start(Stage theStage) {	
-		//model.update(view.currPla);
-    	 //view.update();
-		
- /*	new EventHandler<ActionEvent>() {
- 		@Override 
- 		public void handle(ActionEvent event) {
-            System.out.print("Hello World !!");
-            view.setOnAction(event);
-            }		 	     
-    };
-    */
-    new AnimationTimer() {
-    	
-        public void handle(long currentNanoTime) { 
-            try {
-		model.update(p);
-                Thread.sleep(100);
-                
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        
-    }.start();
+		vp2 = new ViewPage2(theStage);
+
+
     theStage.show();
 }
    
@@ -70,18 +52,17 @@ public class Controller extends Application {
     }
 
 // Mouse Events 
-// Javadoc to follow once fully functioning
-//
 	public void drag(MouseEvent event){
 		Node n = (Node)event.getSource();
 		n.setVisible(true);
 		//model.setX(model.getX() + event.getX());
 		//model.setY(model.getY() + event.getY());
-		model.setX(event.getSceneX() - view.grid.getLayoutX() - view.plaWidth/2); //view.imageView.getFitWidth()/2);
-		model.setY(event.getSceneY() - view.grid.getLayoutY() - view.plaHeight/2);//view.imageView.getFitHeight()/2);
+		model.setX(event.getSceneX() - view.anchor.getLayoutX() - view.plaWidth/2); //view.imageView.getFitWidth()/2);
+		model.setY(event.getSceneY() - view.anchor.getLayoutY() - view.plaHeight/2);//view.imageView.getFitHeight()/2);
 		model.setImage(n);
 		//System.out.println("DRAG");
 	}
+	
 
 	public EventHandler<MouseEvent> getHandlerForDrag(){
 		return event -> drag((MouseEvent) event);
@@ -116,7 +97,7 @@ public class Controller extends Application {
 		view.setI(view.imageView);
 		int plaSize = model.addImageView(view.i);
 		
-		view.moveImageView();
+		view.moveImageView(plaSize);
 		model.addImageView(view.i);
 		model.setPlant(view.getImageName());
 				
@@ -145,8 +126,7 @@ public class Controller extends Application {
 		
 	}
 	
-	public EventHandler getHandlerForClick2(){
-		
+	public EventHandler getHandlerForClick2(){	
 		return event -> click((MouseEvent) event);
 	}
 
