a064c2e74add696390da2d54f6a9a344c133d5df
AmyFeld <58443453+AmyFeld@users.noreply.github.com>
2020-05-19 17:35:31 -0400

    ViewPage4 - remove magic numbers, handles, add javadocs

--- Final Project/src/ViewPage4.java
+++ Final Project/src/ViewPage4.java
@@ -67,7 +67,15 @@ public class ViewPage4 extends View {
 	Slider sl = new Slider(0,3,0);
 	Model m = new Model();
 	ArrayList<Plant> myPlants = new ArrayList<Plant>();
-
+	int second = 2;
+	int fifth = 5;
+	int anchorHeight = 600;
+	int boxRight = 50;
+	double n1 = 0.5; 
+	double n2= 1.5;
+	int min =0; 
+	int max =2; 
+	int tick = 1;
 	   
 	//ArrayList<Plant> page4Plants = new ArrayList<Plant>();
   
@@ -95,23 +103,23 @@ public class ViewPage4 extends View {
 	  	layout1.getChildren().addAll(back, label1, homeButton, rate);
 	     
 	  	//Slider sl = new Slider(0,3,0);
-	      	sl.setOrientation(Orientation.VERTICAL);
-	      	sl.setMin(0);
-	      	sl.setMax(2);
+	    sl.setOrientation(Orientation.VERTICAL);
+	    sl.setMin(min);
+	    sl.setMax(max);
 	      //	sl.setMaxHeight(300);
-	      	sl.setValue(1);
-	      	sl.setShowTickLabels(true);
-	      	sl.setShowTickMarks(true);
-	      	sl.setMajorTickUnit(1);
-	      	sl.setMinorTickCount(0);
-	      	sl.setLabelFormatter(new StringConverter<Double>() {
-	    	  public String toString(Double n) {
-	    		  if (n<0.5) {
+	    sl.setValue(tick);
+	    sl.setShowTickLabels(true);
+	    sl.setShowTickMarks(true);
+	    sl.setMajorTickUnit(tick);
+	    sl.setMinorTickCount(min);
+	    sl.setLabelFormatter(new StringConverter<Double>() {
+	    	public String toString(Double n) {
+	    		  if (n<n1) {
 	    			  //System.out.println("Fall");
 	    			 /// season = "fall";
 	    			  return"Fall";
 	    		  }
-	    		  if(n<1.5) {
+	    		  if(n<n2) {
 	    			//  System.out.println("Spring");
 	    			  //season = "spring";
 	    			  return "Spring";
@@ -121,87 +129,60 @@ public class ViewPage4 extends View {
 	    		  return "Winter";
 	    		  
 	    		  //return "bye";
-	    	  }
+	    	}
 
 			@Override
 			public Double fromString(String s) {
 				switch (s) {
-              			case "Fall":
-                  			return 0d;
-                		case "Spring":
-                    			return 1d;
-                		case "Winter":
-                    			return 2d;
-               
+              		case "Fall":
+                  		return 0d;
+                	case "Spring":
+                    	return 1d;
+                	case "Winter":
+                    	return 2d;
 				}
 				return x;
-
 			}
-			
-			
-		
-	      });
-	      	
-	      
+							
+	    });	      		      
 			
 	      //	System.out.println(season);
 		
-	      Image trash = new Image("file:images/Trash.png", 100, 100, false, false);
-	      ImageView imtrash = new ImageView(trash);
+	    Image trash = new Image("file:images/Trash.png", plaHeight, plaWidth, false, false);
+	    ImageView imtrash = new ImageView(trash);
 	     // Button label = new Button("Create new label");
-	      TextField userlabel = new TextField("Create new label"); 
-	      
-	      //label.setOnAction(new EventHandler<ActionEvent>() {
-	      EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
-	      public void handle(ActionEvent e) {
-	    		  //Label userlabel = new Label(new TextField().getText());
-	    		
-	    		  Label l = new Label();
-	    		  l.setText(userlabel.getText());
-	    		  anchor.getChildren().add(l);
-	    		  l.setOnMouseDragged(control.getHandlerForDrag());
-	    		  l.setOnMouseReleased(control.getHandlerForRelease());
-	    	  }
-	      };
-	      
-	      
-	      Alert explain = new Alert(AlertType.INFORMATION);
-		    explain.setContentText("To add to the garden: click on the plant or object you would like to add it will be added to the garden. Then you can drag to the specific location. To Remove, drag the image to the trashcan. To add labels, type up what you want to add and drag to the location you would like.");
-		    Button tut = new Button(); 
-		    tut.setText("Tutorial");
-		    tut.setOnAction(e -> explain.show() );
+	    userlabel = new TextField("Create new label"); 
+	      	      
+	    Alert explain = new Alert(AlertType.INFORMATION);
+		explain.setContentText("To add to the garden: click on the plant or object you would like to add it will be added to the garden. Then you can drag to the specific location. To Remove, drag the image to the trashcan. To add labels, type up what you want to add and drag to the location you would like.");
+		Button tut = new Button(); 
+		tut.setText("Tutorial");
+		tut.setOnAction(e -> explain.show() );
 		    
-	      userlabel.setOnAction(event);	
-              VBox right = new VBox(50);
-	      right.getChildren().addAll(tut, sl, userlabel, imtrash);
-	  	//scene4 = new Scene(layout1, 900, 600); // the button and label
-		 //   scene1.setFill(Color.LIGHTGREEN);
+	    userlabel.setOnAction(e -> control.label(e));	
+        VBox right = new VBox(boxRight);
+	    right.getChildren().addAll(tut, sl, userlabel, imtrash);
+	  
 	  		  	
-	    homeButton.setOnAction(e -> theStage.setScene(new ViewPage1(theStage).getScene1()));	 
+	    homeButton.setOnAction(e -> control.goHomeButton(theStage, home));	 
 	    rate.setOnAction(e-> {
 	    	m.updateGarden(); 
-	    	theStage.setScene(vp5.getScene5());
+	    	control.goHomeButton(theStage, fifth);
 	    	vp5.setAllRatings(ratings);
 	    
 	    });
-	    back.setOnAction(e -> theStage.setScene(new ViewPage2(theStage).getScene2()));
+	    back.setOnAction(e -> control.goHomeButton(theStage, second ));
 	    
 	    //How To    
-	    
-	    
+	    	    
 		ImageView iv;
 		
 	    start(theStage);
 	    layout1.setAlignment(Pos.TOP_CENTER);
 	    border.setRight(right);
 	    border.setTop(layout1);
-	//	page4Plants = myPlant;
 			
 	    scene4 = new Scene(border, sceneWidth, sceneHeight); // the
-
-		 //tile.setFill(Color.LIGHTGREEN); 
-	  	//tile.setBackground(new Background(new BackgroundFill(Color.web("#" + FFFFFF), CornerRadii.EMPTY, Insets.EMPTY)));
-		
 		  
 	    theStage.setScene(scene4); 
 	    theStage.show(); 	
@@ -216,58 +197,26 @@ public class ViewPage4 extends View {
 	 * @return none  Creates an application displaying the graphics 
 	 *
 	 */
-	public void start(Stage stage) {
-		
-	//Generic Yard Images
-		Image backup_house = new Image("file:images/backup_house.png", 100, 100, false, false);
-		ImageView ivHouse = new ImageView(backup_house);
-		Image pool = new Image("file:images/pool.png", 100, 100, false, false);
-		ImageView ivPool = new ImageView(pool);
-		Image fence = new Image("file:images/fence.png", 150, 50, false, false);
-		ImageView ivFence = new ImageView(fence);
-		Image driveWay= new Image("file:images/road.png", 100, 100, false, false);
-		ImageView ivDriveWay = new ImageView(driveWay);
-		
-	
-		//moving generic images
-		//Generic Image
-		ArrayList<ImageView> genIm = new ArrayList<>();
-		genIm.add(ivHouse);
-		genIm.add(ivPool);
-		genIm.add(ivFence);
-		genIm.add(ivDriveWay);
-		
-		for(int z = 0; z< genIm.size(); z++) {
-			genIm.get(z).setPreserveRatio(true);
-			genIm.get(z).setFitHeight(100);
-	    	
-			genIm.get(z).setOnMousePressed(control.getHandlerForClick2());
-	  	}
-		
+	public void start(Stage stage) {		
+		ArrayList<ImageView> gen = makeGenImg();
 
     	BackgroundImage bg = new BackgroundImage(new Image("file:images/anchor.jpg"), BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT,
 				BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
     	anchor.setBackground(new Background(bg));
 		
 	 	anchor.setStyle("-fx-background-color: #05F861;");
-		
-	//Creating Basic Tile Params
-	 // tile.setTileAlignment(Pos.CENTER_LEFT);
-      //tile.setPrefRows(5);
-      tile.setMaxWidth(plaWidth);
-      tileBox = new VBox(boxSize);
+			
+	 	tile.setMaxWidth(plaWidth);
+	 	tileBox = new VBox(boxSize);
+     
       
-      //create scroll pane :)
+	 	ScrollPane sc = new ScrollPane(tile);
+	 	sc.setPrefViewportHeight(tileBox.getHeight());
+	 	sc.setPrefWidth(plaWidth);
+	 	sc.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+	 	// sc.autosize();
       
-     //setting the anchor and tile to places 
-      
-      ScrollPane sc = new ScrollPane(tile);
-      sc.setPrefViewportHeight(tileBox.getHeight());
-      sc.setPrefWidth(plaWidth);
-      sc.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-     // sc.autosize();
-      
-      	anchor.setPrefHeight(600);
+      	anchor.setPrefHeight(anchorHeight);
     	
       	border.setLeft(sc);
       	border.setCenter(anchor);
@@ -275,16 +224,16 @@ public class ViewPage4 extends View {
     	sl.valueProperty().addListener(
       		new ChangeListener<Number>() {
       			 public String toString(Double n) {
-      		   		  if (n<0.5) {
+      		   		  if (n<n1) {
       		   			  return"fall";
       		   		  }
-      		   		  if(n<1.5) {
+      		   		  if(n<n2) {
       		   			  return "spring";
       		   		  }
       		   		 // System.out.println("Winterzxscccccccdfffffffffffffffffffc");
       		   		 
       		   		  return "winter";
-      		   	  }
+      		   	}
       				 
 				public void changed(ObservableValue<? extends Number> arg0, Number oldVal, Number newVal) {
 					season = toString(newVal.doubleValue());
@@ -295,47 +244,78 @@ public class ViewPage4 extends View {
 					makeIm();
 				    
 
-				} }
-		);
+				} 
+		});
     	     
-      System.out.println("anchor Children: "+ anchor.getChildren());
-      
-    //  tileBox.getChildren().addAll(ivArr);
-      tileBox.getChildren().addAll(genIm);
+    	//System.out.println("anchor Children: "+ anchor.getChildren());
+
+    	tileBox.getChildren().addAll(gen);
      
-      System.out.println(tileBox.getChildren());
-     
-     //  tile.getChildren().add(tileBox);
-       
-      sc.setContent(tileBox);
+    	//System.out.println(tileBox.getChildren());
+    
+    	sc.setContent(tileBox);
 	}
 	
-	
+	/**
+	 * Description: Makes the image view for the plants based off the seasons
+	 */
 	public void makeIm() {
 		
 		ImageView iv;
 		
-		  for( i=0; i< myPlants.size(); i++) {
-	    	  	
-			  	Image im = new Image("file:seasonImages/"+season+"Plants/"+myPlants.get(i).getImgNameGard(), plaWidth, plaHeight, false, false);
-	  	  		iv = new ImageView(im);
+		for( i=0; i< myPlants.size(); i++) {
+	    	 
+		  	Image im = new Image(myPlants.get(i).getImgName(season), plaWidth, plaHeight, false, false);
+	  		iv = new ImageView(im);
 
-	    	  	iv.setPreserveRatio(true);
-	    	  	iv.setFitHeight(100);
-	    	  	
-		      //	Tooltip.install(iv, new Tooltip(control.model.myPlants.get(i).greeneryHover()));
-	//    	  	iv.onMousePressedProperty().bind(e -> control.click(e));
-//	    	  	iv.eventDispatcherProperty().bind(control.getHandlerForClick());
-	    	  	//iv.setOnMousePressed(control.getHandlerForClick());
-		    	iv.setOnMouseClicked(e -> control.click(e));//control.getHandlerForClick());  
+	      	iv.setPreserveRatio(true);
+	      	iv.setFitHeight(plaHeight);
+	    	iv.setOnMouseClicked(e -> control.click(e));//control.getHandlerForClick());  
 		    	
-		    	ivArr.add(i, iv);
+	    	ivArr.add(i, iv);
 	   
-	            } 	
-		  tileBox.getChildren().addAll(ivArr);
+		} 	
+		tileBox.getChildren().addAll(ivArr);
 	}
 
+	/**
+	 * Description: creates all of the generic images to be added to the garden
+	 * @return genIm / ArrayList<ImageView>
+	 */
+	public ArrayList<ImageView> makeGenImg() {
+		//Generic Yard Images
+		Image backup_house = new Image("file:images/backup_house.png", plaHeight, plaWidth, false, false);
+		ImageView ivHouse = new ImageView(backup_house);
+		Image pool = new Image("file:images/pool.png", plaHeight, plaWidth, false, false);
+		ImageView ivPool = new ImageView(pool);
+		Image fence = new Image("file:images/fence.png", plaHeight, plaWidth, false, false);
+		ImageView ivFence = new ImageView(fence);
+		Image driveWay= new Image("file:images/road.png", plaHeight, plaWidth,false, false);
+		ImageView ivDriveWay = new ImageView(driveWay);
+				
 	
+		//moving generic images
+		//Generic Image
+		ArrayList<ImageView> genIm = new ArrayList<>();
+		genIm.add(ivHouse);
+		genIm.add(ivPool);
+		genIm.add(ivFence);
+		genIm.add(ivDriveWay);
+				
+		for(int z = 0; z< genIm.size(); z++) {
+			genIm.get(z).setPreserveRatio(true);
+			genIm.get(z).setFitHeight(plaHeight);
+		   	
+			genIm.get(z).setOnMousePressed(control.getHandlerForClick2());
+		}
+		return genIm;
+				
+	}
+	
+	/**
+	 * Description: takes in an array list to be presented
+	 * @param alst
+	 */
 	public void setMyPlants(ArrayList<Plant> alst) {
 		myPlants = alst;
 		makeIm();
