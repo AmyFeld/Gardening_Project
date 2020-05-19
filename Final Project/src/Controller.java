e284c4ff436ab918cb8fac10e7a727c6d0f50fb0
AmyFeld <58443453+AmyFeld@users.noreply.github.com>
2020-05-19 17:37:36 -0400

    Controller -- added additional handlers

--- Final Project/src/Controller.java
+++ Final Project/src/Controller.java
@@ -1,10 +1,17 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Circle;
@@ -22,15 +29,13 @@ public class Controller extends Application {
 	Model model;
 	double contX;
 	double contY;
+	Scene scene;
+
 	
 	public Controller(View view) {
 		
 		this.view = view;
 		this.model = new Model();
-	}
-	
-	public Controller() {
-		this.model = new Model();	
 	}
 
     /**
@@ -42,8 +47,9 @@ public class Controller extends Application {
      *
      */
     @Override
-	public void start(Stage theStage) {	      
-    theStage.show();
+	public void start(Stage theStage) {	
+    	 
+    	theStage.show();
     }
    
 	public static void main(String[] args) {
@@ -52,15 +58,16 @@ public class Controller extends Application {
     }
 
 // Mouse Events 
+// Javadoc to follow once fully functioning
+//
 	public void drag(MouseEvent event){
 		Node n = (Node)event.getSource();
 		n.setVisible(true);
-		//model.setX(model.getX() + event.getX());
-		//model.setY(model.getY() + event.getY());
 		model.setX(event.getSceneX() - view.anchor.getLayoutX() - view.plaWidth/2); //view.imageView.getFitWidth()/2);
 		model.setY(event.getSceneY() - view.anchor.getLayoutY() - view.plaHeight/2);//view.imageView.getFitHeight()/2);
 		model.setImage(n);
 		view.ratings = model.setRating();
+		//System.out.println("DRAG");
 	}
 
 	public EventHandler<MouseEvent> getHandlerForDrag(){
@@ -75,9 +82,7 @@ public class Controller extends Application {
 		if(n.getTranslateX() > 600) {
 			view.circ = (Circle)n;
 			System.out.println("REMOVE");
-		//	model.setPlant(view.getPlant());
-		//	model.removePlant();
-			//view.imageView = (ImageView)n;
+	
 			view.setI(view.imageView);
 			model.removeImageView(view.i);
 			view.removeImageView();
@@ -108,15 +113,6 @@ public class Controller extends Application {
 		
 	}
 	
-	public void setMyPlants(Plant p) {	
-		model.update(p);
-	}
-	
-	public ArrayList<Plant> getMyPlants() {
-		return model.myPlants;	
-	}
-	
-	
 	public EventHandler<MouseEvent> getHandlerForClick(){
 		return event -> click((MouseEvent) event);
 	}
@@ -127,36 +123,90 @@ public class Controller extends Application {
 			
 		view.imageView = (ImageView)n;
 		view.setI(view.imageView);
+		//model.setPlant(p);
 				
 		view.moveGenImage();
+	
 		
 		System.out.println("click");
 		
 	}
 	
 	public EventHandler getHandlerForClick2(){
+		
 		return event -> click((MouseEvent) event);
 	}
 
-
-	public double getStartingX(){
-		return model.getX();
+	public void setMyPlants(Plant p) {	
+		model.update(p);
 	}
 
-	public double getStartingY(){
-		return model.getY();
-	}
-
-	public double getOriginX(){
-		return contX;
-	}
-
-	public double getOriginY(){
-		return contY;
+	public ArrayList<Plant> getMyPlants() {
+		return model.myPlants;	
 	}
 	
+	public void label(ActionEvent e) {
+		 Label l = new Label();
+		 l.setText(view.userlabel.getText());
+		 view.anchor.getChildren().add(l);
+		 l.setOnMouseDragged(getHandlerForDrag());
+		 l.setOnMouseReleased(getHandlerForRelease());
+	}
+
+	public void chooseFile(ActionEvent e, Stage theStage) {
+		FileChooser fileChooser = new FileChooser();
+  		try {
+  			fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Ser", ".ser"));
+  			File file = fileChooser.showSaveDialog(theStage);
+  			if(file != null) {
+  				FileOutputStream fileOut = new FileOutputStream(file);
+				ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+  				objectOut.writeObject(model);
+				objectOut.close();
+				fileOut.close();
+				
+  			}
+
+		} catch (Exception ex) {
+
+			ex.printStackTrace();}
+	}
+	
+	public void goHomeButton(Stage s, int num) {
+
+		switch (num) {
+		case 1:
+			scene = new ViewPage1(s).getScene1();
+			break;
+		case 2:	
+			scene = new ViewPage2(s).getScene2();
+			break;
+		case 4:	
+			scene = new ViewPage4(s).getScene4();
+			break;
+		case 5:
+			scene = new ViewPage5(s).getScene5();
+			break;
+		case 6:
+			scene = new ViewPage6(s).getScene6();
+			break;
+		case 7:
+			scene = new ViewPage7(s).getScene7();
+			break;
+		case 8:
+			scene = new ViewPage8(s).getScene8();
+			break;
+		case 9:
+			scene = new ViewPage9(s).getScene9();
+			break;				
+		}
+
+		s.setScene(scene);
+	}
 	public ArrayList<Integer> getRating() {
 		return model.setRating();
 	}
+	
+	
 
 }
